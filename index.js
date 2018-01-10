cce55cb99825502290a6d380676e50ef2943cedd
Cutina Woof <gretchenista@gmail.com>
2018-01-10 16:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan";
-var height = " is less than 40";
+var height = 39;
 var message = ` gives the ${name} and ${height}`;
 
 module.exports = {name, height, message};
